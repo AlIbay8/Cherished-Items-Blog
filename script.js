615b046be974590bccdce661450e4aa7373813c2
AlIbay8 <43456952+AlIbay8@users.noreply.github.com>
2018-12-12 17:55:39 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".imgMail").click(function() {
+    $(".imgMail").hide();
+    $(".qr").show();
+});
+
+$(".phone").click(function() {
+    $("#highlightG").show();
+    $("#highlightB").show();
+    $("#highlightP").show();
+    $("#highlightR").show();
+});
